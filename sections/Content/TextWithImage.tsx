fc1e05fb7d55cf2825807d8fec21a57db4c99d56
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2024-03-26 09:03:26 -0300

    feat(text-with-image): created new component text-with-image

--- sections/Content/TextWithImage.tsx
+++ sections/Content/TextWithImage.tsx
@@ -1,66 +1,114 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import type { HTMLWidget } from "apps/admin/widgets.ts";
 
-export interface ServiceProps {
-  type?: string;
-  label?: string;
-  description?: HTMLWidget;
-  image: ImageWidget;
-  placement: "left" | "right";
-}
-
 export interface Props {
-  services?: ServiceProps[];
+  title?: HTMLWidget;
+  titleMobile?: HTMLWidget;
+  description?: HTMLWidget;
+  image?: {
+    mobile?: ImageWidget;
+    desktop?: ImageWidget;
+  };
+  placement: "left" | "right";
+  style: {
+    textAlign?: "left" | "center" | "right";
+    backgroundColor?: string;
+    fontColor?: string;
+  };
+  CTA?: {
+    label?: string;
+    href?: string;
+    backgroundColor?: string;
+    fontColor?: string;
+    alignment?: "left" | "center" | "right";
+  };
 }
 
 const PLACEMENT = {
-  left: "flex-col lg:flex-row-reverse",
-  right: "flex-col lg:flex-row",
+  left: "lg:order-1",
+  right: "lg:order-0",
 };
 
-export default function Services({
-  services = [
-    {
-      type: "Service",
-      label: "Your Title Here",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus, elit arcu ultricies massa, quis ornare nisl libero vitae urna.",
-      image:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/3290/488e5dc5-9a24-48c9-9795-09b97394fb5f",
-      placement: "left",
-    },
-  ],
+export default function TextWithImage({
+  title = "Lorem ipsum",
+  titleMobile = "Lorem ipsum dolor",
+  description =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices ornare ultrices. Vestibulum gravida ligula nec ex scelerisque, sed tristique neque porttitor. ",
+  image = {
+    mobile: "https://placehold.co/350x390",
+    desktop: "https://placehold.co/704x704",
+  },
+  placement = "left",
+  style = {
+    textAlign: "left",
+    backgroundColor: "#CAC7B6",
+    fontColor: "#000",
+  },
+  CTA = {
+    label: "Saiba mais",
+    href: "#saiba-mais",
+    backgroundColor: "#FFC72C",
+    fontColor: "#000",
+    alignment: "center",
+  },
 }: Props) {
   return (
-    <div class="bg-base-100 flex flex-col py-28">
-      {services?.map((service, index) => (
-        <div
-          key={index}
-          class={`flex xl:container xl:mx-auto first:pt-0 py-12 lg:py-28 mx-5 md:mx-10 ${
-            PLACEMENT[service.placement]
-          } gap-12 md:gap-20 text-left items-center justify-evenly`}
-        >
-          <img
-            class="w-full lg:w-1/2 flex-1 object-cover rounded-xl aspect-[31/21]"
-            sizes="(max-width: 640px) 100vw, 30vw"
-            src={service.image}
-            alt={service.label}
-            decoding="async"
-            loading="lazy"
+    <div
+      class={`
+      relative grid grid-cols-1 lg:grid-cols-2 items-center font-poppins
+      text-${style.textAlign}
+    `}
+      style={{ backgroundColor: style.backgroundColor, color: style.fontColor }}
+    >
+      <div class={`h-full ${PLACEMENT[placement]}`}>
+        <img
+          class="w-full object-cover block md:hidden"
+          src={image.mobile}
+          alt={title}
+          decoding="async"
+          loading="lazy"
+        />
+        <img
+          class="w-full h-full object-cover max-h-[704px] hidden md:block"
+          src={image.desktop}
+          alt={title}
+          decoding="async"
+          loading="lazy"
+        />
+      </div>
+      <div class="absolute lg:relative w-full h-full flex-1 flex flex-col items-center justify-end lg:justify-center order-0">
+        <div class="flex flex-col p-5 py-14 max-w-[430px] gap-3">
+          <h3
+            class="text-2xl block lg:hidden text-center text-white"
+            dangerouslySetInnerHTML={{ __html: titleMobile || "" }}
           />
-          <div class="w-full lg:w-1/2 flex-1 space-y-2 lg:space-y-4 lg:max-w-xl gap-4">
-            {service.type && <p>{service.type}</p>}
-            <p class="text-base-content text-[36px] md:text-[48px] leading-[120%] font-bold">
-              {service.label}
-            </p>
-            <p
-              class="text-base-content text-[16px] md:text-[18px]"
-              dangerouslySetInnerHTML={{ __html: service.description || "" }}
+          <h3
+            class="text-4xl hidden lg:block"
+            dangerouslySetInnerHTML={{ __html: title || "" }}
+          />
+          <span
+            class="text-xl hidden lg:block"
+            dangerouslySetInnerHTML={{ __html: description || "" }}
+          />
+          {CTA?.href && (
+            <a
+              href={CTA.href}
+              class={`
+                btn text-sm mt-5 py-2.5 px-3.5 w-fit rounded-none border-transparent self-center
+                ${CTA.alignment === "left" && "lg:self-start"}
+                ${CTA.alignment === "center" && "lg:self-center"}
+                ${CTA.alignment === "right" && "lg:self-end"}
+              `}
+              style={{
+                backgroundColor: CTA.backgroundColor,
+                color: CTA.fontColor,
+              }}
             >
-            </p>
-          </div>
+              {CTA.label}
+            </a>
+          )}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
