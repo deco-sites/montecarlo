f90c9364d15bbd4afbb4fd81eaa023557e0283cc
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2024-04-02 15:54:56 -0300

    feat(newsletter): separate the newsletter from the footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,7 +7,6 @@ import MobileApps from "../../components/footer/MobileApps.tsx";
 import PaymentMethods from "../../components/footer/PaymentMethods.tsx";
 import RegionSelector from "../../components/footer/RegionSelector.tsx";
 import Social from "../../components/footer/Social.tsx";
-import Newsletter from "../../islands/Newsletter.tsx";
 import { clx } from "../../sdk/clx.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import PoweredByDeco from "apps/website/components/PoweredByDeco.tsx";
@@ -71,7 +70,6 @@ export interface Layout {
     | "Variation 5";
   hide?: {
     logo?: boolean;
-    newsletter?: boolean;
     sectionLinks?: boolean;
     socialLinks?: boolean;
     paymentMethods?: boolean;
@@ -86,12 +84,6 @@ export interface Props {
   logo?: {
     image: ImageWidget;
     description?: string;
-  };
-  newsletter?: {
-    title?: string;
-    /** @format textarea */
-    description?: string;
-    form?: NewsletterForm;
   };
   sections?: Section[];
   social?: {
@@ -121,11 +113,6 @@ const LAYOUT = {
 
 function Footer({
   logo,
-  newsletter = {
-    title: "Newsletter",
-    description: "",
-    form: { placeholder: "", buttonText: "", helpText: "" },
-  },
   sections = [{
     "label": "Sobre",
     "items": [
@@ -176,7 +163,6 @@ function Footer({
     variation: "Variation 1",
     hide: {
       logo: false,
-      newsletter: false,
       sectionLinks: false,
       socialLinks: false,
       paymentMethods: false,
@@ -188,15 +174,6 @@ function Footer({
   },
 }: Props) {
   const _logo = layout?.hide?.logo ? <></> : <Logo logo={logo} />;
-  const _newsletter = layout?.hide?.newsletter ? <></> : (
-    <Newsletter
-      content={newsletter}
-      layout={{
-        tiled: layout?.variation == "Variation 4" ||
-          layout?.variation == "Variation 5",
-      }}
-    />
-  );
   const _sectionLinks = layout?.hide?.sectionLinks ? <></> : (
     <FooterItems
       sections={sections}
@@ -233,7 +210,6 @@ function Footer({
             <div class="flex flex-col md:flex-row md:justify-between md:flex-wrap lg:flex-nowrap gap-8 lg:gap-12">
               {_logo}
               {_sectionLinks}
-              {_newsletter}
             </div>
             <Divider />
             <div class="flex flex-col md:flex-row gap-10 md:gap-14 md:items-end">
@@ -262,7 +238,6 @@ function Footer({
                 {_region}
               </div>
               <div class="flex flex-col gap-10 lg:gap-20 lg:w-1/2 lg:pr-10">
-                {_newsletter}
                 {_sectionLinks}
               </div>
             </div>
@@ -278,7 +253,6 @@ function Footer({
             {_logo}
             <div class="flex flex-col lg:flex-row gap-14">
               <div class="flex flex-col md:flex-row lg:flex-col md:justify-between lg:justify-normal gap-10 lg:w-2/5">
-                {_newsletter}
                 <div class="flex flex-col gap-10">
                   {_payments}
                   {_apps}
@@ -301,8 +275,6 @@ function Footer({
         )}
         {layout?.variation == "Variation 4" && (
           <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
             <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 lg:justify-between">
               {_sectionLinks}
               <div class="flex flex-col md:flex-row lg:flex-col gap-10 lg:gap-10 lg:w-2/5 lg:pl-10">
@@ -329,8 +301,6 @@ function Footer({
         )}
         {layout?.variation == "Variation 5" && (
           <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
             {_logo}
             <div class="flex flex-col md:flex-row gap-10 lg:gap-20 md:justify-between">
               {_sectionLinks}
