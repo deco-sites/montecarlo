4dae43ac49ab0942fbf4222a9c3c94318e1ed7ed
ApMatheus <matheusaparecido101@gmail.com>
2024-04-08 09:48:01 -0300

    feat(SearchStore) data static

--- loaders/SearchStore.tsx
+++ loaders/SearchStore.tsx
@@ -21,7 +21,8 @@ export interface Props {
 
 /**@title Search Store */
 const loader = async (): Promise<Data> => {
-  const link = "/api/dataentities/NL/search?_fields=city,state,uf";
+  const link =
+    "https://www.montecarlo.com.br/api/dataentities/NL/search?_fields=city,state,uf";
 
   const settings = {
     method: "GET",
@@ -33,6 +34,57 @@ const loader = async (): Promise<Data> => {
 
   const storesMD: LocationStore[] = await fetch(link, settings).then((data) => {
     return data.json();
+  }).catch((error) => {
+    return [
+      { city: "Campinas", state: "São Paulo", uf: "SP" },
+      { city: "Curitiba", state: "Paraná", uf: "PR" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Belo Horizonte", state: "Minas Gerais", uf: "MG" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Balneário Camboriú", state: "Santa Catarina", uf: "SC" },
+      { city: "Fortaleza", state: "Ceará", uf: "CE" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "São Carlos", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "São José dos Campos", state: "São Paulo", uf: "SP" },
+      { city: "Salvador", state: "Bahia", uf: "BA" },
+      { city: "Guará", state: "Brasília", uf: "DF" },
+      { city: "Santos", state: "São Paulo", uf: "SP" },
+      { city: "Belo Horizonte", state: "Minas Gerais", uf: "MG" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Ribeirão Preto", state: "São Paulo", uf: "SP" },
+      { city: "Goiânia", state: "Goiás", uf: "GO" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Santo André", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Campinas", state: "São Paulo", uf: "SP" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "São Caetano do Sul", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Barueri", state: "São Paulo", uf: "SP" },
+      { city: "Recife", state: "Pernambuco", uf: null },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Cuiabá", state: "Mato Grosso", uf: "MT" },
+      { city: "Barueri", state: "São Paulo", uf: "SP" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Vitória", state: "Espirito Santo", uf: "ES" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Belo Horizonte", state: "Minas Gerais", uf: "MG" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Rio de Janeiro", state: "Rio de Janeiro", uf: "RJ" },
+      { city: "Campo Grande", state: "Mato Grosso do Sul", uf: "MS" },
+      { city: "São Paulo", state: "São Paulo", uf: "SP" },
+      { city: "Niterói", state: "Rio de Janeiro", uf: "RJ" },
+    ];
   });
 
   const states: Array<States> = [];
