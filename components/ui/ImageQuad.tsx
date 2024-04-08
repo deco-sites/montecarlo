7ad66d5efe063656ca310537f47bdc63316e9859
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-04-08 08:17:24 -0300

    refactor(comercial-page): run lint

--- components/ui/ImageQuad.tsx
+++ components/ui/ImageQuad.tsx
@@ -28,35 +28,63 @@ export interface Props {
   fourthImage: Image;
 }
 
-
 interface Wrapper {
-  children: ComponentChildren
+  children: ComponentChildren;
 }
 
 function WrapperImage({ children }: Wrapper) {
-
   return (
     <div class="relative w-full md:w-2/4">
-      { children }
+      {children}
 
-      <div class="absolute overflow-y-hidden w-full h-full bg-gradient-to-t from-[#01010157] to-transparent top-0"></div>
+      <div class="absolute overflow-y-hidden w-full h-full bg-gradient-to-t from-[#01010157] to-transparent top-0">
+      </div>
     </div>
-  )
+  );
 }
 
-
-export default function ImageQuad({ firstImage, secondImage, thirdImage, fourthImage }: Props) {
+export default function ImageQuad(
+  { firstImage, secondImage, thirdImage, fourthImage }: Props,
+) {
   return (
-      <div class={'container flex flex-col py-8 sm:flex-wrap sm:flex-row'} >
+    <div class={"container flex flex-col py-8 sm:flex-wrap sm:flex-row"}>
+      <WrapperImage>
+        <SectionImage
+          props={firstImage}
+          customClass={"mt-[-2px]"}
+          wrapperCustomClass={`w-full lg:w-full`}
+          infoSectionCustomClass={`absolute top-0 z-10`}
+        />
+      </WrapperImage>
+
+      <WrapperImage>
+        <SectionImage
+          props={secondImage}
+          customClass={"mt-[-2px]"}
+          wrapperCustomClass={`w-full lg:w-full`}
+          infoSectionCustomClass={`absolute top-0 z-10`}
+        />
+      </WrapperImage>
+      {thirdImage && (
         <WrapperImage>
-          <SectionImage props={firstImage} customClass={'mt-[-2px]'} wrapperCustomClass={`w-full lg:w-full`} infoSectionCustomClass={`absolute top-0 z-10`} />
+          <SectionImage
+            props={thirdImage}
+            customClass={"mt-[-2px]"}
+            wrapperCustomClass={`w-full lg:w-full`}
+            infoSectionCustomClass={`absolute top-0 z-10`}
+          />
         </WrapperImage>
-        
+      )}
+      {fourthImage && (
         <WrapperImage>
-          <SectionImage props={secondImage} customClass={'mt-[-2px]'} wrapperCustomClass={`w-full lg:w-full`} infoSectionCustomClass={`absolute top-0 z-10`} />
+          <SectionImage
+            props={fourthImage}
+            customClass={"mt-[-2px]"}
+            wrapperCustomClass={`w-full lg:w-full`}
+            infoSectionCustomClass={`absolute top-0 z-10`}
+          />
         </WrapperImage>
-        { thirdImage && <WrapperImage><SectionImage props={thirdImage} customClass={'mt-[-2px]'} wrapperCustomClass={`w-full lg:w-full`} infoSectionCustomClass={`absolute top-0 z-10`} /></WrapperImage> }
-        { fourthImage && <WrapperImage><SectionImage props={fourthImage} customClass={'mt-[-2px]'} wrapperCustomClass={`w-full lg:w-full`} infoSectionCustomClass={`absolute top-0 z-10`} /></WrapperImage> }
-      </div>
-  )
+      )}
+    </div>
+  );
 }
