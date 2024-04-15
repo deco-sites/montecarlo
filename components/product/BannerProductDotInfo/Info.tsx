f13cdb05a85e68c4c08273eec54039d1fc361cbf
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2024-04-15 12:04:10 -0300

    feat(product-dot-info): corrects info display condition

--- components/product/BannerProductDotInfo/Info.tsx
+++ components/product/BannerProductDotInfo/Info.tsx
@@ -25,7 +25,7 @@ export default function Info({ alignment, coordinates, productData }: Props) {
       onClick={handleInfo}
       class={`flex ${x} ${y} absolute invisible bg-white group-hover:visible group-data-[active=true]:visible opacity-0 group-hover:opacity-100 group-data-[active=true]:opacity-100 group-hover:z-10 group-data-[active=true]:z-10 transition-opacity w-max items-center cursor-pointer`}
     >
-      {productData && productData[0].image && (
+      {productData && productData[0]?.image && (
         <div class="h-full">
           <Image
             class="w-28 h-auto max-w-[20vw] object-cover"
@@ -36,7 +36,7 @@ export default function Info({ alignment, coordinates, productData }: Props) {
         </div>
       )}
 
-      {productData && productData[0].productName && (
+      {productData && productData[0]?.productName && (
         <div class="flex flex-col justify-center p-1 md:p-4 gap-1 font-poppins text-[11px] md:text-sm text-black max-w-[30vw] md:max-w-48 max-h-28">
           <span class="line-clamp-2">
             {productData[0].productName}
