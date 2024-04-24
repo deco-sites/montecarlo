83372efdc4e27272fc37993947acf0a29e9dcf26
Guilherme Noé <guilherme.snoe@gmail.com>
2024-04-24 01:39:16 -0300

    fix:item clique footer

--- components/footer/ItemFooter.tsx
+++ components/footer/ItemFooter.tsx
@@ -10,33 +10,33 @@ export interface Props {
 const ItemFooter = ({ href, label }: Props) => {
   return (
     <>
-    <a
-      id='item-footer'
-      href={href}
-      onClick={(e) => {
-        e.preventDefault();
-        // EventsClick({
-        //   event: "click_menu",
-        //   props: {
-        //     menu_name: label,
-        //     menu_url: href,
-        //   },
-        // });
-      }}
-      className="block py-1 link link-hover text-xs"
-    >
-      {label}
-    </a>
+      <a
+        id="item-footer"
+        href={href}
+        onClick={(e) => {
+          e.preventDefault();
+          // EventsClick({
+          //   event: "click_menu",
+          //   props: {
+          //     menu_name: label,
+          //     menu_url: href,
+          //   },
+          // });
+        }}
+        className="block py-1 link link-hover text-xs"
+      >
+        {label}
+      </a>
       <SendEventOnClick
-          id="item-footer"
-          event={{
-            name: "select_promotion",
-            params: {
-              item_list_name: label,
-              items: [],
-            },
-          }}
-    />
+        id="item-footer"
+        event={{
+          name: "select_promotion",
+          params: {
+            item_list_name: label,
+            items: [],
+          },
+        }}
+      />
     </>
   );
 };
