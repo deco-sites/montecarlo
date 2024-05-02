7f4d22063e6950790a850a71252a86ac45723954
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2024-05-02 07:35:30 -0300

    feat(hero-text-with-image): created component HeroTextWithImage

---
+++ sections/Content/HeroTextWithImage.tsx
@@ -0,0 +1,126 @@
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import type { HTMLWidget } from "apps/admin/widgets.ts";
+
+interface StyleProps {
+    textAlign?: "left" | "center" | "right";
+    /** @format color-input */
+    backgroundColor?: string;
+    /** @format color-input */
+    fontColor?: string;    
+}
+
+interface CTAProps {
+  label?: string;
+  href?: string;
+  /** @format color-input */
+  backgroundColor?: string;
+  /** @format color-input */
+  fontColor?: string;
+}
+
+export interface Props {
+  title?: string;
+  description?: HTMLWidget;
+  image?: {
+    mobile?: ImageWidget;
+    desktop?: ImageWidget;
+  };
+  placement: "left" | "right";
+  style: StyleProps;
+  CTA?: {
+    alignment?: "left" | "center" | "right";
+    buttons?: CTAProps[];
+  };
+}
+
+const PLACEMENT = {
+  left: "lg:order-1",
+  right: "lg:order-0",
+};
+
+export default function HeroTextWithImage({
+  title = "Lorem ipsum",
+  description = "Lorem ipsum dolor sit amet consectetur",
+  image = {
+    mobile: "https://placehold.co/350x390",
+    desktop: "https://placehold.co/704x704",
+  },
+  placement = "left",
+  style = {
+    textAlign: "left",
+    backgroundColor: "#CAC7B6",
+    fontColor: "#000",
+  },
+  CTA = {
+    alignment: "right",
+    buttons: [
+      {
+        label: "Saiba mais",
+        href: "#saiba-mais",
+        backgroundColor: "#FFC72C",
+        fontColor: "#000",
+      },
+    ],
+  },
+}: Props) {
+  return (
+    <div
+      class={`
+      relative grid grid-cols-1 lg:grid-cols-2 items-center font-poppins
+      text-${style.textAlign}
+    `}
+      style={{ backgroundColor: style.backgroundColor, color: style.fontColor }}
+    >
+      <div class={`h-full ${PLACEMENT[placement]}`}>
+        <img
+          class="w-full object-cover block md:hidden"
+          src={image.mobile}
+          alt={title}
+          decoding="async"
+          loading="eager"
+        />
+        <img
+          class="w-full h-full object-cover max-h-[704px] hidden md:block"
+          src={image.desktop}
+          alt={title}
+          decoding="async"
+          loading="eager"
+        />
+      </div>
+      <div class="w-full h-full flex-1 flex flex-col items-center justify-end lg:justify-center order-0">
+        <div class="flex flex-col p-5 py-14 max-w-[380px] gap-3">
+          <h1 class="text-4xl lg:text-[3.438rem] block text-left leading-12 font-beausiteGrand">
+            {title}
+          </h1>
+          <p class="text-xl leading-9 font-poppins">
+            {description}
+          </p>
+          <div
+            class={`
+                flex border border-white w-fit mt-5
+                ${
+                  (CTA?.alignment === "left" && "justify-start") ||
+                  (CTA?.alignment === "center" && "justify-center") ||
+                  (CTA?.alignment === "right" && "justify-end")
+                }
+            `}
+          >
+            {CTA?.buttons?.map((cta, index) => (
+              <a
+                key={index}
+                href={cta.href}
+                class={`btn text-xl py-2.5 px-3.5 w-fit rounded-none border-transparent self-center hover:opacity-80 font-light`}
+                style={{
+                  backgroundColor: cta.backgroundColor,
+                  color: cta.fontColor,
+                }}
+              >
+                {cta.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
