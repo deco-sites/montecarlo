c367f3170f016fdc72cdb8b27d3ec0e1b7824226
Guilherme Noé <55108063+guilhermenoe@users.noreply.github.com>
2024-07-10 18:37:38 -0300

    fix: adjustament-popup view action page

--- components/newsletter-popup/ButtonClosePopup.tsx
+++ components/newsletter-popup/ButtonClosePopup.tsx
@@ -1,5 +1,9 @@
 import type { ComponentChildren } from "preact";
 import { useUI } from "deco-sites/montecarlo/sdk/useUI.ts";
+import { useSignalEffect } from "@preact/signals";
+import { useRef } from "preact/compat";
+
+
 
 export interface Props {
   children: ComponentChildren;
@@ -8,9 +12,32 @@ export interface Props {
 export default function ClosedPopup({ children }: Props) {
   const { showPopup } = useUI();
 
+  const refContainerPopup = useRef<HTMLDivElement>(null);
+
+
+  useSignalEffect(() => {
+    const show = sessionStorage.getItem("showpopup");
+
+    const activePopup = () => {
+      const show = sessionStorage.getItem("showpopup");
+
+      if (show == null || show == "") {
+        showPopup.value = true;
+      } else {
+        globalThis.window.removeEventListener("mousemove", activePopup)
+      }
+    }
+
+    if (show == null || show == "") {
+      globalThis.window.addEventListener("mousemove", activePopup)
+    } else {
+      globalThis.window.removeEventListener("mousemove", activePopup)
+    }
+  });
+
   // setTimeout(() => {
   //   showPopup.value = false;
   // }, 25000);
 
-  return <>{showPopup.value ? <div>{children}</div> : null}</>;
+  return <>{showPopup.value ? <div ref={refContainerPopup}>{children}</div> : null}</>;
 }
