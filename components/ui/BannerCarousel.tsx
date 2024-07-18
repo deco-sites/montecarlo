6bf1b5d8f443d63db9af93adc93ff51f7fe9fe7f
ApMatheus <matheusaparecido101#gmail.com>
2024-07-18 20:07:29 -0300

    fix: format code replace

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -240,19 +240,6 @@ function BannerItem({
               },
             }}
           />
-          id={id}
-          event={{
-            name: "view_promotion",
-            params: {
-              view_promotion: primaryImage.promotion,
-              crative_name: primaryImage.alt,
-              creative_slot: "banner-carousel",
-              promotion_id: id,
-              promotion_name: primaryImage.promotion,
-              items: [],
-            },
-          }}
-          />
         </div>
       ))}
     </div>
