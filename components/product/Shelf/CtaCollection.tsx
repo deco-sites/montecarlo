24b9f0da5782de00956e7b47a0614fd52aa36e10
Rafael Ribeiro <rafael.araujo.dev@hotmail.com>
2024-08-06 12:54:59 -0300

    feat(cta-colletcion): corrects cta button width

--- components/product/Shelf/CtaCollection.tsx
+++ components/product/Shelf/CtaCollection.tsx
@@ -10,12 +10,13 @@ function CtaCollection(
   { ctaCollection, hrefCollection, showOnDesktop, showOnMobile, customClass }:
     Props,
 ) {
+
   if (ctaCollection) {
     return (
       <a
         href={hrefCollection || ""}
         aria-label="view product"
-        class={`w-min py-[10px] px-[14px] bg-primary text-black text-sm  ${
+        class={`py-[10px] px-[14px] bg-primary text-black text-sm  ${
           showOnMobile ? "block" : "hidden"
         } lg:px-4 lg:py-2 lg:text-lg lg:${showOnDesktop ? "block" : "hidden"} ${
           customClass ? customClass : ""
